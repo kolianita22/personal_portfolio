9d97f435cd80b0dc0d466581a3fa0ef33e1b2989
kolianita22 <kolianita784@gmail.com>
2025-12-20 16:06:47 +0530

    delete some files

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,8 @@ from mysite import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='base'),
+    path('home/ ', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('resume/', views.resume, name='resume'),
     path('project/', views.project, name='project'),
--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from mysite.models import Contact
 
 
 # Create your views here.
 def home(request):
+    return render(request, 'home.html')
 
-     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
 
