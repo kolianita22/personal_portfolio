8d5ec0e857bc3a186bfeb7f4a94719d62e168752
kolianita22 <kolianita784@gmail.com>
2026-01-02 13:27:53 +0530

    updeted setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,6 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CSRF_TRUSTED_ORIGINS = [
+    'https://personalportfolio-production-7f99.up.railway.app'
+]
 
 
 # Quick-start development settings - unsuitable for production
